b0e6b6b91b625c5deb719cc3bdaae35350207d68
Daryyya <darya9841402670@gmail.com>
2022-10-24 19:02:01 +0300

    add likes(checkboxes)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import Header from "./Header";
 import Main from "./Main";
 import { data } from "./hp.js";
 import Card from "./Card";
+import { useState } from "react";
 
 function App() {
+  const [localst, setLocalst] = useState([])
+  const [ch, setCh] = useState('');
+
+  console.log(localst)
   return (
     <>
       <Header>
         <Form />
       </Header>
       <Main>
-        {data.map(({ actor, name, gender, house, wand, image, alive }) => (
-          <Card
+        {data.map(({ actor, name, gender, house, wand, image, alive,  }) => (
+          <Card setCh={setCh}
+          setLocalst={setLocalst}
+          ch={ch}
+          localst={localst}
             key={name}
             image={image}
             actor={actor}
--- src/Card/index.jsx
+++ src/Card/index.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
-import style from './style.module.scss'
+import React from "react";
+import style from "./style.module.scss";
 
-const Card = ({image, actor, name, gender, house, wand, alive}) => {
+const Card = ({
+  image,
+  actor,
+  name,
+  gender,
+  house,
+  wand,
+  alive,
+  setCh,
+  setLocalst,
+  localst,
+  ch
+}) => {
   return (
     <div className={style.card}>
-        <div className={style.card__img}>
-            <img className={style.image} src={image} width={334} height={192}/>
-        </div>
-        <div className={style.card__wrapper}>
-            <h3 className={style.title}>{name}</h3>
-            <p className={style.card__info}>{`Actor: ${actor}`}</p>
-            <p className={style.card__info}>{`Gender: ${gender}`}</p>
-            <p className={style.card__info}>{`House: ${house}`}</p>
-            <p className={style.card__info}>{`Wand core: ${wand}`}</p>
-            <p className={style.card__info}>{`Alive: ${alive ? 'yes' : 'no'}`}</p>
-        </div>
+      <label
+        onChange={(event) => {
+          setCh(event.target.checked);
+          if (ch) {
+            setLocalst([...localst, {key: actor}])
+          }
+          
+        }}
+        className={style.check}
+      >
+        <input className={style.check__input} type="checkbox" />
+        <span className={style.check__box}></span>
+      </label>
+      <div className={style.card__img}>
+        <img className={style.image} src={image} width={334} height={192} />
+      </div>
+      <div className={style.card__wrapper}>
+        <h3 className={style.title}>{name}</h3>
+        <p className={style.card__info}>{`Actor: ${actor}`}</p>
+        <p className={style.card__info}>{`Gender: ${gender}`}</p>
+        <p className={style.card__info}>{`House: ${house}`}</p>
+        <p className={style.card__info}>{`Wand core: ${wand}`}</p>
+        <p className={style.card__info}>{`Alive: ${alive ? "yes" : "no"}`}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
